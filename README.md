0a006aa5792f2e32a8c15370741361811103fde2
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-04 16:22:29 +0900

    first commit for creating repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# SSDA - 함께쓰는 일기장으로 상대와 일상을 공유해보세요
+
+## Frontend Repository
+
+안녕하세요, SSDA Frontend Repository입니다.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ display: 'grid', placeContent: 'center', width: '100dvw', height: '100dvh' }}>
+      <h1>This is SSDA Initial Test Page</h1>
+    </div>
+  );
+}
+
+export default App;
