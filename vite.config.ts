12c532646cb89a7ebb36302abe3e48f052bf6774
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-11 14:09:04 +0900

    [DIAR-92] conf: Vite 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,22 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@APIs', replacement: '/src/APIs' },
+      { find: '@Components', replacement: '/src/Components' },
+      { find: '@Hooks', replacement: '/src/Hooks' },
+      { find: '@Icons', replacement: '/src/Icons' },
+      { find: '@Layouts', replacement: '/src/Layouts' },
+      { find: '@Pages', replacement: '/src/Pages' },
+      { find: '@Routes', replacement: '/src/Routes' },
+      { find: '@Store', replacement: '/src/Store' },
+      { find: '@Styles', replacement: '/src/Styles' },
+      { find: '@Types', replacement: '/src/Types' },
+      { find: '@Utils', replacement: '/src/Utils' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
   css: {
     modules: {
       localsConvention: 'camelCase',
