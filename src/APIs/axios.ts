e92495b9d6c9659d01bebfa2a5912ad36a3763af
boyon99 <bo_yon_99@naver.com>
2024-01-14 11:31:15 +0900

    Conf: axios 환경 설정

--- src/APIs/axios.ts
+++ src/APIs/axios.ts
@@ -1,10 +1,31 @@
-import Axios, { type CreateAxiosDefaults } from 'axios';
+import Axios, { type CreateAxiosDefaults, AxiosError } from 'axios';
 
-const axiosConfig: CreateAxiosDefaults = {
-  baseURL: '',
-  timeout: 2500,
+const getAxiosInstance = () => {
+  const axiosConfig: CreateAxiosDefaults = {
+    baseURL: '',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // CORS 문제 해결
+    },
+    withCredentials: true,
+    timeout: 2500,
+  };
+
+  const axios = Axios.create(axiosConfig);
+
+  axios.interceptors.request.use(
+    // 요청이 성공했을 때 실행될 함수
+    (config) => {
+      return config;
+    },
+    // 요청이 실패했을 때 실행될 함수
+    (error: AxiosError) => {
+      console.log('axios request error', error);
+      return Promise.reject(error);
+    },
+  );
+
+  return axios;
 };
 
-const axios = Axios.create(axiosConfig);
-
-export { axios };
+export const axios = getAxiosInstance;
---
+++ src/APIs/index.ts
@@ -0,0 +1,7 @@
+// import { axios } from './axios';
+
+// export const createPost = async (post: Post) => {
+//   const res = await axios().post<Post>('/posts', post);
+//   console.log(res.data);
+//   return res.data;
+// };
