b84b8279335eaccda683123652d5f9af75d27e92
boyon99 <bo_yon_99@naver.com>
2024-01-18 11:00:53 +0900

    feat: 모달 공통 컴포넌트 레이아웃 및 닫기 기능 추가

---
+++ src/Components/Common/Modal/Modal.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss';
+
+interface ModalProps {
+  setClose: React.Dispatch<React.SetStateAction<boolean>>;
+  content: React.ReactNode;
+  title: string;
+  footer: React.ReactNode;
+}
+
+export const Modal = ({ setClose, content, title, footer }: ModalProps) => {
+  return (
+    <div className={styles.fullContainer}>
+      <div className={styles.header}>
+        <h3>{title}</h3>
+        <button onClick={() => setClose(false)}>X</button>
+      </div>
+      <div className={styles.content}>{content}</div>
+      <div className={styles.footer}>{footer}</div>
+    </div>
+  );
+};
