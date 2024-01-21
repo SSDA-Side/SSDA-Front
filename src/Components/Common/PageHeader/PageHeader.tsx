ec129f3ec030f4dee72ea18369f74d7b08b2c854
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-21 23:13:14 +0900

    [DIAR-94] feat: 공통 컴포넌트 PageHeader 생성

---
+++ src/Components/Common/PageHeader/PageHeader.tsx
@@ -0,0 +1,31 @@
+/** Style */
+import styles from './PageHeader.module.scss';
+
+/** Type */
+import type { PropsWithChildren } from 'react';
+
+type LeftSectionProp = PropsWithChildren;
+const LeftSection = ({ children }: LeftSectionProp) => {
+  return <div className={styles.group}>{children}</div>;
+};
+
+type CenterSectionProp = PropsWithChildren;
+const CenterSection = ({ children }: CenterSectionProp) => {
+  return <div className={styles.center}>{children}</div>;
+};
+
+type RightSectionProp = PropsWithChildren;
+const RightSection = ({ children }: RightSectionProp) => {
+  return <div className={`${styles.group} ${styles.right}`}>{children}</div>;
+};
+
+type PageHeaderProp = PropsWithChildren;
+const PageHeader = ({ children }: PageHeaderProp) => {
+  return <div className={styles.container}>{children}</div>;
+};
+
+PageHeader.Left = LeftSection;
+PageHeader.Center = CenterSection;
+PageHeader.Right = RightSection;
+
+export { PageHeader };
