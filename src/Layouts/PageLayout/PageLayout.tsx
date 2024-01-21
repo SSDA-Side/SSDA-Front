ef1d085919ce58463d1e4a640c8f30482fb18ca9
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-21 23:52:56 +0900

    [DIAR-94] feat: PageLayout에 Footer 추가

--- src/Layouts/PageLayout/PageLayout.tsx
+++ src/Layouts/PageLayout/PageLayout.tsx
@@ -1,16 +1,21 @@
-import { ReactNode } from 'react';
+/** Style */
 import styles from './PageLayout.module.scss';
+
+/** Type */
+import type { ReactNode } from 'react';
 
 type PageLayoutProp = {
   header: ReactNode;
   body: ReactNode;
+  footer?: ReactNode;
 };
 
-export const PageLayout = ({ header, body }: PageLayoutProp) => {
+export const PageLayout = ({ header, body, footer }: PageLayoutProp) => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>{header}</div>
       <div className={styles.body}>{body}</div>
+      {footer && <div className={styles.footer}>{footer}</div>}
     </div>
   );
 };
