0585adc222f68e45e009d9a8210786faf495e33c
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-21 23:18:36 +0900

    [DIAR-94] feat: MY 일기장 HeroSection 개발

--- src/Pages/MyBoardPage/MyBoardPage.tsx
+++ src/Pages/MyBoardPage/MyBoardPage.tsx
@@ -31,5 +31,30 @@ const Head = () => {
 };
 
 const Body = () => {
-  return <main className={styles.contaienr}>{/* TODO */}</main>;
+  return (
+    <main className={styles.contaienr}>
+      <HeroSection />
+    </main>
+  );
+};
+
+const HeroSection = () => {
+  const heroData = {
+    username: '도토리',
+    sharedDiaryCount: 13,
+    sharedPeopleCount: 3,
+  };
+
+  const username = heroData?.username || '-';
+  const sharedPeopleCount = heroData?.sharedPeopleCount || '-';
+  const sharedDiaryCount = heroData?.sharedDiaryCount || '-';
+
+  return (
+    <section className={styles.heroSection}>
+      <Typography as="h1">{`${username}님,\n소중한 일상을 공유해보세요`}</Typography>
+      <Typography as="body2" className={styles.description}>
+        지금까지 {sharedPeopleCount}명과 {sharedDiaryCount}개의 일기를 공유했어요
+      </Typography>
+    </section>
+  );
 };
