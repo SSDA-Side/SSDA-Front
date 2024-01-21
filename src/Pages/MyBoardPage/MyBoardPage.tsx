399f0b2eba28141da4e6f1a5569cb8dc1024c348
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-21 23:17:01 +0900

    [DIAR-94] feat: MY 일기장 헤더 개발

--- src/Pages/MyBoardPage/MyBoardPage.tsx
+++ src/Pages/MyBoardPage/MyBoardPage.tsx
@@ -1,5 +1,35 @@
+/** Style 및 Layout */
 import { PageLayout } from '@Layouts/PageLayout';
+import styles from './MyBoardPage.module.scss';
+
+/** Component */
+import { IconButton } from '@Components/Common/Button';
+import { PageHeader } from '@Components/Common/PageHeader';
+import { Typography } from '@Components/Common/Typography';
 
 export const MyBoardPage = () => {
-  return <PageLayout header={<h1>My 일기장 Header</h1>} body={<p>This is MyBoardPage</p>} />;
+  return <PageLayout header={<Head />} body={<Body />} />;
+};
+
+const Head = () => {
+  return (
+    <PageHeader>
+      <PageHeader.Left>
+        <IconButton icon="menu" />
+      </PageHeader.Left>
+
+      <PageHeader.Center>
+        <Typography as="h4">MY 일기장</Typography>
+      </PageHeader.Center>
+
+      <PageHeader.Right>
+        <IconButton icon="header/bell" />
+        <IconButton icon="user" />
+      </PageHeader.Right>
+    </PageHeader>
+  );
+};
+
+const Body = () => {
+  return <main className={styles.contaienr}>{/* TODO */}</main>;
 };
