8cd70e4ae94fbb048eab68dbc37263bc1986bad7
boyon99 <bo_yon_99@naver.com>
2024-01-22 22:27:54 +0900

    feat: 아이콘으로 변경 및 스타일 전역 속성 적용하기

--- src/Components/Common/Modal/Modal.tsx
+++ src/Components/Common/Modal/Modal.tsx
@@ -1,21 +1,22 @@
+import { SVGIcon } from '@Icons/SVGIcon';
 import styles from './Modal.module.scss';
 
 type ModalProps = {
   setClose: React.Dispatch<React.SetStateAction<boolean>>;
   content: React.ReactNode;
   title: string;
-  footer: React.ReactNode;
 };
 
-export const Modal = ({ setClose, content, title, footer }: ModalProps) => {
+export const Modal = ({ setClose, content, title }: ModalProps) => {
   return (
     <div className={styles.fullContainer}>
       <div className={styles.header}>
         <h3>{title}</h3>
-        <button onClick={() => setClose(false)}>X</button>
+        <button onClick={() => setClose(false)}>
+          <SVGIcon name="close" />
+        </button>
       </div>
       <div className={styles.content}>{content}</div>
-      <div className={styles.footer}>{footer}</div>
     </div>
   );
 };
