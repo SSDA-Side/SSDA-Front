ce6b0df664332edcf663de95d23865986ed53622
boyon99 <bo_yon_99@naver.com>
2024-01-22 14:02:36 +0900

    [DIAR-94] feat: InputBox 스타일 작업 및 기능 구현
    - 최대 글자 수 표시

--- src/Components/Common/InputBox/InputBox.tsx
+++ src/Components/Common/InputBox/InputBox.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
 import styles from './InputBox.module.scss';
 
-export const InputBox = () => {
-  return <div className={styles.fullContainer}>This is InputBox</div>;
+type InputBoxProps = {
+  placeHolder: string;
+};
+
+export const InputBox = ({ placeHolder }: InputBoxProps) => {
+  const [inputCount, setInputCount] = useState<number>(0);
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputCount(() => e.target.value.length);
+  };
+
+  return (
+    <div className={styles.fullContainer}>
+      <input
+        type="text"
+        className={styles.inputBox}
+        placeholder={placeHolder}
+        maxLength={10}
+        onChange={onInputChange}
+      />
+      <p>
+        <span>{inputCount}</span>
+        <span>/10</span>
+      </p>
+    </div>
+  );
 };
