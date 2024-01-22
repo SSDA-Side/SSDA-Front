8a227a18f7b237171715e21fadf5aa35b88f3d54
boyon99 <bo_yon_99@naver.com>
2024-01-22 12:16:48 +0900

    [DIAR-93] feat: 동일한 인스턴스 생성
    동일한 인스턴스를 반환하도록 코드 수정

--- src/APIs/axios.ts
+++ src/APIs/axios.ts
@@ -1,31 +1,25 @@
 import Axios, { type CreateAxiosDefaults, AxiosError } from 'axios';
 
-const getAxiosInstance = () => {
-  const axiosConfig: CreateAxiosDefaults = {
-    baseURL: '',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*', // CORS 문제 해결
-    },
-    withCredentials: true,
-    timeout: 2500,
-  };
-
-  const axios = Axios.create(axiosConfig);
-
-  axios.interceptors.request.use(
-    // 요청이 성공했을 때 실행될 함수
-    (config) => {
-      return config;
-    },
-    // 요청이 실패했을 때 실행될 함수
-    (error: AxiosError) => {
-      console.log('axios request error', error);
-      return Promise.reject(error);
-    },
-  );
-
-  return axios;
+const axiosConfig: CreateAxiosDefaults = {
+  baseURL: 'https://jsonplaceholder.typicode.com/',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*', // CORS 문제 해결
+  },
+  withCredentials: true,
+  timeout: 2500,
 };
 
-export const axios = getAxiosInstance;
+export const axios = Axios.create(axiosConfig);
+
+axios.interceptors.request.use(
+  // 요청이 성공했을 때 실행될 함수
+  (config) => {
+    return config;
+  },
+  // 요청이 실패했을 때 실행될 함수
+  (error: AxiosError) => {
+    console.log('axios request error', error);
+    return Promise.reject(error);
+  },
+);
