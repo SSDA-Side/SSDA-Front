9ca2b736e1683ca33c9ee515121e98563937275f
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-26 17:22:00 +0900

    [DIAR-99] feat: Confirm 모달 기능 생성 전역으로 Confirm 컴포넌트 관리 Hook 생성(useConfirm) RootLayout에서 관리

--- src/Layouts/RootLayout/RootLayout.tsx
+++ src/Layouts/RootLayout/RootLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
 import styles from './RootLayout.module.scss';
+import { Confirm } from '@Components/Common/Confirm';
 
 export const RootLayout = () => {
   return (
     <div className={styles.fullContainer}>
       <div className={styles.appContaienr}>
         <Outlet />
+        <Confirm />
       </div>
     </div>
   );
