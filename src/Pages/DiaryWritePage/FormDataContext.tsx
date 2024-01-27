c5b2eb67960bea14d9728f3a4e8004060ba0954d
sangpok <48979587+sangpok@users.noreply.github.com>
2024-01-27 16:41:01 +0900

    [DIAR-99] feat: Form Data 관련 Context 작성 API 명세 전이라 임의 타입 지정

---
+++ src/Pages/DiaryWritePage/FormDataContext.tsx
@@ -0,0 +1,106 @@
+import { createContext, useContext, useReducer } from 'react';
+
+type WriteFormDate = {
+  boardId: string;
+  diaryTitle: string;
+  contents: string;
+  stickerId: string;
+  diaryDate: string;
+  diaryImgs: FileList | null;
+};
+
+const initialFormData: WriteFormDate = {
+  boardId: '-1',
+  diaryTitle: '',
+  contents: '',
+  stickerId: '0',
+  diaryDate: '',
+  diaryImgs: null,
+};
+
+const FormDataContext = createContext<WriteFormDate | null>(null);
+const FormDataDispatchContext = createContext<React.Dispatch<any> | null>(null);
+
+const formDataReducer = (state, action) => {
+  switch (action.type) {
+    case 'updateFields': {
+      return { ...state, ...action.payload };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+export const FormDataContextProvider = ({ children }) => {
+  const [formData, dispatch] = useReducer(formDataReducer, initialFormData);
+
+  return (
+    <FormDataContext.Provider value={formData}>
+      <FormDataDispatchContext.Provider value={dispatch}>{children}</FormDataDispatchContext.Provider>
+    </FormDataContext.Provider>
+  );
+};
+
+export const useFormDataState = () => {
+  const context = useContext(FormDataContext);
+
+  if (context === null) {
+    throw new Error('never but for type-guard');
+  }
+
+  return context;
+};
+
+export const useFormDataDispatch = () => {
+  const context = useContext(FormDataDispatchContext);
+
+  if (context === null) {
+    throw new Error('never but for type-guard');
+  }
+
+  return context;
+};
+
+export const useFormData = () => {
+  const dispatch = useFormDataDispatch();
+
+  const updateBoardId = (boardId: string) =>
+    dispatch({
+      type: 'updateFields',
+      payload: { boardId },
+    });
+
+  const updateDiaryDate = (diaryDate: string) =>
+    dispatch({
+      type: 'updateFields',
+      payload: { diaryDate },
+    });
+
+  const updateEmotionId = (stickerId: string) =>
+    dispatch({
+      type: 'updateFields',
+      payload: { stickerId },
+    });
+
+  const updateTitle = (diaryTitle: string) =>
+    dispatch({
+      type: 'updateFields',
+      payload: { diaryTitle },
+    });
+
+  const updateContents = (contents: string) =>
+    dispatch({
+      type: 'updateFields',
+      payload: { contents },
+    });
+
+  const updateImages = (diaryImgs: FileList) =>
+    dispatch({
+      type: 'updateFields',
+      payload: { diaryImgs },
+    });
+
+  return { updateBoardId, updateDiaryDate, updateEmotionId, updateTitle, updateContents, updateImages };
+};
