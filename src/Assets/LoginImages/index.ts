47b70207c751581a6e3ce69a1f2ad3c918be9c24
boyon99 <bo_yon_99@naver.com>
2024-01-30 23:02:56 +0900

    [DIAR-98] design: 이미지 컴포넌트화하기

--- src/Assets/LoginImages/index.ts
+++
@@ -1,7 +0,0 @@
-import love from './love.svg';
-import book from './book.svg';
-import bubbleOne from './bubble-01.png';
-import bubbleTwo from './bubble-02.png';
-
-export const loginImage = [book, love];
-export const bubbleImage = [bubbleOne, bubbleTwo];
