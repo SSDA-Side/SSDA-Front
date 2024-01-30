7616e3955f2a1783d4db5f1d320ce113ec9a4861
boyon99 <bo_yon_99@naver.com>
2024-01-30 21:37:36 +0900

    design: 말풍선 이미지 추가

--- src/Assets/LoginImages/index.ts
+++ src/Assets/LoginImages/index.ts
@@ -1,4 +1,7 @@
 import love from './love.svg';
 import book from './book.svg';
+import bubbleOne from './bubble-01.png';
+import bubbleTwo from './bubble-02.png';
 
 export const loginImage = [book, love];
+export const bubbleImage = [bubbleOne, bubbleTwo];
