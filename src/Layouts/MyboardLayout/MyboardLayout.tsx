04544718d86cc78ca0409b78855b701a2e1c1ce5
boyon99 <bo_yon_99@naver.com>
2024-02-02 13:33:42 +0900

    [DIAR-105] feat: tab bar 버튼 기능 구현 및 디자인 적용

--- src/Layouts/MyboardLayout/MyboardLayout.tsx
+++ src/Layouts/MyboardLayout/MyboardLayout.tsx
@@ -1,11 +1,41 @@
 /** Style */
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './MyboardLayout.module.scss';
+import cn from 'classnames';
+
+const tabList = [
+  {
+    name: '캘린더',
+    path: 'calendar',
+  },
+  {
+    name: '모아보기',
+    path: 'all',
+  },
+  {
+    name: '새글',
+    path: 'new',
+  },
+];
 
 export const MyboardLayout = () => {
+  const navigate = useNavigate();
+  const boardId = '1';
+  const location = useLocation();
+
   return (
     <div className={styles.container}>
-      <div className={styles.tabBar}></div>
+      <div className={styles.tabBar}>
+        {tabList.map((tab) => (
+          <button
+            key={tab.path}
+            onClick={() => navigate(`/myboard/${tab.path}/${boardId}`)}
+            className={cn({ [styles.active]: location.pathname.includes(tab.path) })}
+          >
+            {tab.name}
+          </button>
+        ))}
+      </div>
       <Outlet />
     </div>
   );
