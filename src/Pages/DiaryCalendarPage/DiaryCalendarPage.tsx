43ad8319df686a764862687b038904f5576985c4
boyon99 <bo_yon_99@naver.com>
2024-02-02 13:33:12 +0900

    [DIAR-107] design: 캘린더 페이지 디자인 적용

--- src/Pages/DiaryCalendarPage/DiaryCalendarPage.tsx
+++ src/Pages/DiaryCalendarPage/DiaryCalendarPage.tsx
@@ -1,5 +1,10 @@
 import { Calendar } from '@Components/Calendar';
+import styles from './DiaryCalendarPage.module.scss';
 
 export const DiaryCalendarPage = () => {
-  return <Calendar />;
+  return (
+    <div className={styles.container}>
+      <Calendar />
+    </div>
+  );
 };
