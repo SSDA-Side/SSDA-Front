67c5059231c8da031f65c577c92b818c873a781d
sangpok <48979587+sangpok@users.noreply.github.com>
2024-02-03 23:57:22 +0900

    [DIAR-106] conf: env Typescript 작성

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_KAKAO_KEY: string;
+  readonly VITE_KAKAO_REST_API_KEY: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
+  readonly VITE_FRONT_BASE_URI: string;
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
