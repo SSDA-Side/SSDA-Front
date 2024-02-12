ce6b9c6185af02eadb4859ce44411a93b39bc6c8
boyon99 <bo_yon_99@naver.com>
2024-02-12 16:57:05 +0900

    [DIAR-109] feat: 각 버튼 클릭 시 해당 페이지로 이동

--- src/Pages/SettingPage/SettingPage.tsx
+++ src/Pages/SettingPage/SettingPage.tsx
@@ -1,3 +1,26 @@
+import { SVGIcon } from '@Icons/SVGIcon';
+import { settingList } from '@Layouts/SettingLayout/SettingLayout';
+import styles from './Setting.module.scss';
+import { useNavigate } from 'react-router-dom';
+
 export const SettingPage = () => {
-  return <div>This is SettingPage</div>;
+  const navigate = useNavigate();
+
+  const onPageMove = (page: string) => {
+    navigate(page);
+  };
+
+  return (
+    <div className={styles.container}>
+      {settingList.map((setting, index) => {
+        if (index === 5) return;
+        return (
+          <button key={index} className={styles.settingItem} onClick={() => onPageMove(setting.path)}>
+            {setting.name}
+            <SVGIcon name="right" />
+          </button>
+        );
+      })}
+    </div>
+  );
 };
