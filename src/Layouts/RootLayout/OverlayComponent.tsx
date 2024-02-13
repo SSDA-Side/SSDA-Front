4cf9a1a1138fd6e8ea2560812ae5a8df23b5303a
sangpok <48979587+sangpok@users.noreply.github.com>
2024-02-13 12:55:26 +0900

    [DIAR-110] feat: Overlay 관련 컴포넌트 작성 Modal, Sheet, Confirm, Alert 공통 Layout 작성 Controller 추가 -> appContainer 내

---
+++ src/Layouts/RootLayout/OverlayComponent.tsx
@@ -0,0 +1,185 @@
+import { IconButton } from '@Components/Common/Button';
+import { PageHeader } from '@Components/Common/PageHeader';
+import { Typography } from '@Components/Common/Typography';
+import { useModal } from '@Hooks/useModal';
+import { PageLayout } from '@Layouts/PageLayout';
+import cn from 'classnames';
+import { PropsWithChildren } from 'react';
+import { createPortal } from 'react-dom';
+import styles from './OverlayComponent.module.scss';
+import { AlertPayload, ComponentPayload, ConfirmPayload } from '@Store/ModalStore';
+import { BottomSheetType } from '@Store/SheetShore';
+import { useSheet } from '@Hooks/useSheet';
+
+type ModalProp = PropsWithChildren<{ isOpened: boolean; onClose: () => void }>;
+const Modal = ({ isOpened, onClose, children }: ModalProp) => {
+  const body = document.querySelector('body')!;
+
+  return (
+    isOpened &&
+    createPortal(
+      <div className={styles.modalContainer} onClick={onClose}>
+        {children}
+      </div>,
+      body,
+    )
+  );
+};
+
+const Overlay = () => {
+  return <div className={styles.overlay} />;
+};
+
+const Content = ({ children }: PropsWithChildren) => {
+  return (
+    <div className={styles.content} onClick={(e) => e.stopPropagation()}>
+      {children}
+    </div>
+  );
+};
+
+const ConfirmAlertBox = ({ children }: PropsWithChildren) => {
+  return (
+    <div className={styles.confirmAlertBox} onClick={(e) => e.stopPropagation()}>
+      {children}
+    </div>
+  );
+};
+
+const BottomSheetBox = ({ children }: PropsWithChildren) => {
+  return (
+    <div className={styles.bottomSheetBox} onClick={(e) => e.stopPropagation()}>
+      {children}
+    </div>
+  );
+};
+
+Modal.Overlay = Overlay;
+Modal.Content = Content;
+Modal.Confirm = ConfirmAlertBox;
+Modal.Alert = ConfirmAlertBox;
+Modal.BottomSheet = BottomSheetBox;
+
+type ComponentModalProp = { id: string; isOpened: boolean } & ComponentPayload;
+export const ComponentModal = ({ id, isOpened, ...payload }: ComponentModalProp) => {
+  const { closeModal, openAlert } = useModal();
+
+  const { children: ModalElement, title, onClose, isSubmitting } = payload;
+
+  const handleClose = () => {
+    closeModal(id);
+    onClose && onClose();
+  };
+
+  const header = (
+    <PageHeader>
+      <PageHeader.Center>
+        <Typography as="h2" onClick={() => openAlert({ contents: '그래 인마!' })}>
+          {title}
+        </Typography>
+      </PageHeader.Center>
+
+      <PageHeader.Right>
+        <IconButton icon="close" onClick={handleClose} disabled={isSubmitting} />
+      </PageHeader.Right>
+    </PageHeader>
+  );
+
+  return (
+    <Modal isOpened={isOpened} onClose={handleClose}>
+      <Modal.Overlay />
+      <Modal.Content>
+        <PageLayout header={header} body={<ModalElement modalId={id} />} />
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+type AlertProp = { id: string; isOpened: boolean } & AlertPayload;
+export const Alert = ({ id, isOpened, ...payload }: AlertProp) => {
+  const { closeModal } = useModal();
+
+  const { contents, onYes } = payload;
+
+  const handleYes = () => {
+    closeModal(id);
+    onYes && onYes();
+  };
+
+  return (
+    <Modal isOpened={isOpened} onClose={() => {}}>
+      <Modal.Overlay />
+      <Modal.Alert>
+        <div className={styles.contentBox}>{contents}</div>
+        <div className={styles.buttons}>
+          <button onClick={handleYes}>예</button>
+        </div>
+      </Modal.Alert>
+    </Modal>
+  );
+};
+
+type ConfirmProp = { id: string; isOpened: boolean } & ConfirmPayload;
+export const Confirm = ({ id, isOpened, ...payload }: ConfirmProp) => {
+  const { closeModal } = useModal();
+
+  const { contents, isReversedOrder, onNo, onYes } = payload;
+
+  const handleYes = () => {
+    closeModal(id);
+    onYes && onYes();
+  };
+
+  const handleNo = () => {
+    closeModal(id);
+    onNo && onNo();
+  };
+
+  return (
+    <Modal isOpened={isOpened} onClose={() => {}}>
+      <Modal.Overlay />
+      <Modal.Confirm>
+        <div className={styles.contentBox}>{contents}</div>
+        <div className={cn({ [styles.reverseButton]: isReversedOrder }, styles.buttons)}>
+          <button onClick={handleYes}>예</button>
+          <button onClick={handleNo}>아니오</button>
+        </div>
+      </Modal.Confirm>
+    </Modal>
+  );
+};
+
+export const BottomSheet = ({ id, isOpened, ...payload }: BottomSheetType) => {
+  const { openAlert } = useModal();
+  const { closeBottomSheet } = useSheet();
+
+  const { children: SheetElement, title, onClose } = payload;
+
+  const handleClose = () => {
+    closeBottomSheet();
+    onClose && onClose();
+  };
+
+  const header = (
+    <PageHeader>
+      <PageHeader.Center>
+        <Typography as="h2" onClick={() => openAlert({ contents: '그래 인마!' })}>
+          {title}
+        </Typography>
+      </PageHeader.Center>
+
+      <PageHeader.Right>
+        <IconButton icon="close" onClick={handleClose} />
+      </PageHeader.Right>
+    </PageHeader>
+  );
+
+  return (
+    <Modal isOpened={isOpened} onClose={handleClose}>
+      <Modal.Overlay />
+      <Modal.BottomSheet>
+        <PageLayout header={header} body={<SheetElement sheetId={id} />} />
+      </Modal.BottomSheet>
+    </Modal>
+  );
+};
--- src/Layouts/RootLayout/RootLayout.tsx
+++ src/Layouts/RootLayout/RootLayout.tsx
@@ -1,15 +1,46 @@
+import { useModal } from '@Hooks/useModal';
+import { AlertPayload, ComponentPayload, ConfirmPayload, Modal } from '@Store/ModalStore';
 import { Outlet } from 'react-router-dom';
-
+import { Alert, BottomSheet, ComponentModal, Confirm } from './OverlayComponent';
 import styles from './RootLayout.module.scss';
-import { Confirm } from '@Components/Common/Confirm';
+import { useSheet } from '@Hooks/useSheet';
 
 export const RootLayout = () => {
   return (
     <div className={styles.fullContainer}>
       <div className={styles.appContaienr}>
         <Outlet />
-        <Confirm />
+        <ModalController />
+        <SheetController />
       </div>
     </div>
   );
+};
+
+const ModalController = () => {
+  const { modals } = useModal();
+
+  const getTypedComponent = ({ id, type, isOpened, payload }: Modal) => {
+    if (type === 'Component') {
+      return <ComponentModal id={id} isOpened={isOpened} {...(payload as ComponentPayload)} />;
+    }
+
+    if (type === 'Alert') {
+      return <Alert id={id} isOpened={isOpened} {...(payload as AlertPayload)} />;
+    }
+
+    if (type === 'Confirm') {
+      return <Confirm id={id} isOpened={isOpened} {...(payload as ConfirmPayload)} />;
+    }
+  };
+
+  return modals.map(getTypedComponent);
+};
+
+const SheetController = () => {
+  const { bottomSheet } = useSheet();
+
+  const { isOpened, ...bottomSheetProps } = bottomSheet;
+
+  return <BottomSheet isOpened={isOpened} {...bottomSheetProps} />;
 };
