9c929090800186356bca12a3223da14d825b2847
sangpok <48979587+sangpok@users.noreply.github.com>
2024-02-14 22:04:23 +0900

    [DIAR-112] feat: 무한스크롤 관련 Hook 작성

---
+++ src/Hooks/useInfiniteObserver.ts
@@ -0,0 +1,47 @@
+import React, { useEffect, useMemo } from 'react';
+
+export const useInfiniteObserver = ({
+  parentNodeId,
+  onIntersection,
+}: {
+  parentNodeId: string;
+  onIntersection: () => void;
+}) => {
+  const intersectionObserver = useMemo(
+    () =>
+      new IntersectionObserver(
+        ([intersection]) => {
+          const { isIntersecting } = intersection;
+          isIntersecting && onIntersection();
+        },
+        { threshold: 0.1 },
+      ),
+
+    [],
+  );
+
+  const mutationObserver = useMemo(
+    () =>
+      new MutationObserver((mutations) => {
+        const [lastNode] = mutations.pop()!.addedNodes;
+
+        intersectionObserver.disconnect();
+        intersectionObserver.observe(lastNode as Element);
+      }),
+    [],
+  );
+
+  const disconnect = () => {
+    mutationObserver.disconnect();
+    intersectionObserver.disconnect();
+  };
+
+  useEffect(() => {
+    mutationObserver.observe(document.getElementById(parentNodeId)!, { childList: true });
+    intersectionObserver.observe(document.getElementById(parentNodeId)!);
+
+    return () => disconnect();
+  }, []);
+
+  return { disconnect };
+};
