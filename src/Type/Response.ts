e6993cb5b0f92c72912accf494d3d35bcd58e6f2
boyon99 <bo_yon_99@naver.com>
2024-02-14 15:58:45 +0900

    [DIAR-108] feat: 상세 다이어리 API 데이터 타입 작성

--- src/Type/Response.ts
+++ src/Type/Response.ts
@@ -4,3 +4,24 @@ export type HeroData = {
   hasSharedBoard: boolean;
 } & Pick<Board, 'memberCount' | 'diaryCount'> &
   Pick<Member, 'nickname'>;
+
+export type DiaryDetailData = {
+  commentCount: number;
+  contents: string;
+  emotionId: number;
+  id: number;
+  images: DiaryDetailImage[];
+  likeCount: number;
+  memberId: number;
+  nickname: string;
+  owned: boolean;
+  profileUrl: string;
+  regDate: string;
+  selectDate: string;
+  title: string;
+};
+
+type DiaryDetailImage = {
+  id: number;
+  imgUrl: string;
+};
