4e1bbcb061858cf2fdb29eee0c907ea8f77c7f5a
boyon99 <bo_yon_99@naver.com>
2024-02-17 16:56:49 +0900

    fix: API에서 같은 데이터가 다른 이름으로 오는 문제 해결

--- src/Components/DiaryItem/DiaryItem.tsx
+++ src/Components/DiaryItem/DiaryItem.tsx
@@ -7,13 +7,7 @@ import { useNavigate } from 'react-router-dom';
 
 export const DiaryItem = ({ diary }: { diary: todayDiaryData }) => {
   const navigate = useNavigate();
-  const selectDay =
-    diary.selectDate !== undefined
-      ? diary.selectDate.split('T')[0]
-      : diary.selectedDate !== undefined
-        ? diary.selectedDate.split('T')[0]
-        : '';
-
+  const selectDay = diary.selectedDate !== undefined ? diary.selectedDate.split('T')[0] : '';
   return (
     <button
       className={styles.container}
