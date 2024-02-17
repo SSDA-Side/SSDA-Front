499809b6c073cfeba9ae782c56e99b980f004af4
boyon99 <bo_yon_99@naver.com>
2024-02-17 16:57:25 +0900

    [DIAR-108] feat: Myboard 공통 레이아웃에 일기 상세보기 페이지 추가 및 예외처리

--- src/Layouts/MyboardLayout/MyboardLayout.tsx
+++ src/Layouts/MyboardLayout/MyboardLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './MyboardLayout.module.scss';
 import cn from 'classnames';
 import { useIsNewDiary } from '@Hooks/NetworkHooks';
+import { SVGIcon } from '@Icons/SVGIcon';
 
 const tabList = [
   {
@@ -22,27 +23,48 @@ const tabList = [
 export const MyboardLayout = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const diaryLocation = location.pathname.split('/')[2];
   const boardId = location.pathname.split('/')[3];
 
+  const date = location.pathname.split('/').slice(3, 4);
+  const [year, month, day] = date[0].split('-');
+
+  const isDiaryDetail = diaryLocation === 'calendar' || diaryLocation === 'all' || diaryLocation === 'new';
   const { data: isNewDiary, isError, isSuccess } = useIsNewDiary(Number(boardId));
 
   isError && console.error('isNewDiaryData error', isError);
 
+  const onBeforePage = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.tabBar}>
-        {tabList.map((tab) => (
-          <button
-            key={tab.path}
-            onClick={() => navigate(`/myboard/${tab.path}/${boardId}`)}
-            className={cn({ [styles.active]: location.pathname.includes(tab.path) })}
-          >
-            {tab.name}
-            {isNewDiary?.newExist && tab.path === 'new' && <div className={styles.newDiary} />}
-          </button>
-        ))}
+    <>
+      <div className={styles.header}>
+        <button onClick={onBeforePage}>
+          <SVGIcon name="left" />
+        </button>
+        <div>{!isDiaryDetail ? `${year}년 ${month}월 ${day}일` : '일기장 제목'}</div>
       </div>
-      <Outlet />
-    </div>
+      {isDiaryDetail ? (
+        <div className={styles.container}>
+          <div className={styles.tabBar}>
+            {tabList.map((tab) => (
+              <button
+                key={tab.path}
+                onClick={() => navigate(`/myboard/${tab.path}/${boardId}`)}
+                className={cn({ [styles.active]: location.pathname.includes(tab.path) })}
+              >
+                {tab.name}
+                {isNewDiary?.newExist && tab.path === 'new' && <div className={styles.newDiary} />}
+              </button>
+            ))}
+          </div>
+          <Outlet />
+        </div>
+      ) : (
+        <Outlet />
+      )}
+    </>
   );
 };
