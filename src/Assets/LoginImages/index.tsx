83db16559c7bffbbe7b2bc2074b91c0f79857400
boyon99 <bo_yon_99@naver.com>
2024-02-17 18:17:17 +0900

    design: 이미지 스타일 변경

--- src/Assets/LoginImages/index.tsx
+++ src/Assets/LoginImages/index.tsx
@@ -3,12 +3,22 @@ import book from './login_book.png';
 import bubbleOne from './login_bubble01.png';
 import bubbleTwo from './login_bubble02.png';
 
-export const LoginImage = ({ index }: { index: number }) => {
+type LoginImageProps = {
+  index: number;
+};
+
+export const LoginImage = ({ index }: LoginImageProps) => {
   const loginImage = [book, love];
   return <img src={loginImage[index]} alt="login-carousel-image" style={{ width: '185px', height: '185px' }} />;
 };
 
-export const BubbleImage = ({ index }: { index: number }) => {
+export const BubbleImage = ({ index }: LoginImageProps) => {
   const bubbleImage = [bubbleOne, bubbleTwo];
-  return <img src={bubbleImage[index]} alt="login-carousel-bubble" style={{ width: '313px', height: '153px' }} />;
+  return (
+    <img
+      src={bubbleImage[index]}
+      alt="login-carousel-bubble"
+      style={{ width: '313px', height: '153px', marginLeft: '1rem' }}
+    />
+  );
 };
