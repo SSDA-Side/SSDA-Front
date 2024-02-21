13bf4e49b284b60bae37d5d8ca6927b3f811d0dc
boyon99 <bo_yon_99@naver.com>
2024-02-21 14:37:54 +0900

    [DIAR-109] feat: 피드백 페이지 버튼 예외처리

--- src/Pages/SettingFeedbackPage/SettingFeedbackPage.tsx
+++ src/Pages/SettingFeedbackPage/SettingFeedbackPage.tsx
@@ -3,10 +3,12 @@ import { SVGIcon } from '@Icons/SVGIcon';
 import { useState } from 'react';
 import styles from './SettingFeedbackPage.module.scss';
 import { CTAButton } from '@Components/Common/Button';
+import cn from 'classnames';
 
 // TODO: [feat] 의견 보내기 기능 구현
 export const SettingFeedbackPage = () => {
   const [rating, setRating] = useState(0);
+  const [feedback, setFeedback] = useState('');
 
   return (
     <div className={styles.container}>
@@ -24,10 +26,26 @@ export const SettingFeedbackPage = () => {
             </button>
           ))}
         </div>
-        <TextArea placeholder="DASSDA를 사용하시면서 좋았던 점, 불편하셨던 점 등 소중한 의견을 입력해주세요." />
+        <TextArea
+          placeholder="DASSDA를 사용하시면서 좋았던 점, 불편하셨던 점 등 소중한 의견을 입력해주세요."
+          value={feedback}
+          onChange={(e) => setFeedback(e.target.value)}
+        />
       </div>
-      <div className={styles.buttonBox}>
-        <CTAButton>의견 보내기</CTAButton>
+      <div
+        className={cn(styles.buttonBox, {
+          [styles.active]: feedback.length > 0 && rating > 0,
+        })}
+      >
+        <CTAButton
+          onClick={() => {
+            if (feedback.length > 0 && rating > 0) {
+              console.log('의견 보내기');
+            }
+          }}
+        >
+          의견 보내기
+        </CTAButton>
       </div>
     </div>
   );
